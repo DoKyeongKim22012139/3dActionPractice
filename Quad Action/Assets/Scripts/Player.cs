335b025ef29bab9a53ec5523b57f39f868ef0491
DoKyeong Kim <kjkkjs5252@gmail.com>
2025-07-07 03:21:39 +0900

    점프랑 구르기
    점프랑 구르기

--- Quad Action/Assets/Scripts/Player.cs
+++ Quad Action/Assets/Scripts/Player.cs
@@ -10,9 +10,15 @@ public class Player : MonoBehaviour
     float hAxis;
     float vAxis;
     bool wDown;
+    bool jDown;
 
 
+    bool isJump;
+    bool isDodge;
+
+    Rigidbody rigid;
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
     Animator anime;
 
@@ -21,6 +27,7 @@ public class Player : MonoBehaviour
     {
         
         anime=GetComponentInChildren<Animator>();
+        rigid=GetComponent<Rigidbody>();
     }
 
     void Start()
@@ -31,24 +38,82 @@ public class Player : MonoBehaviour
     
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+    }
+
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+    }
 
+     void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * speed*(wDown ? 0.3f :1f)*Time.deltaTime;
-        
+        if(isDodge)
+        {
+            moveVec = dodgeVec;
+        }
+        transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
+
 
         anime.SetBool("isRun", moveVec != Vector3.zero);
         anime.SetBool("isWalk", wDown);
-
-
-
-
-        transform.LookAt(transform.position+moveVec);
-
-
-
     }
+
+    void Turn()
+    {
+        transform.LookAt(transform.position + moveVec);
+    }
+
+
+    void Jump()
+    {
+        if(jDown && moveVec == Vector3.zero && !isJump &&!isDodge)
+        {
+            rigid.AddForce(Vector3.up*20, ForceMode.Impulse);
+            anime.SetBool("isJump", true);
+            anime.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+           
+            anime.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.4f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed  *= 0.5f;
+        isDodge = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag =="Floor")
+        {
+            anime.SetBool("isJump", false);
+            isJump = false;
+        }
+    }
+
 }
