2a9cdfeff63c9216d790c856a8369d24d8a185b1
DoKyeong Kim <kjkkjs5252@gmail.com>
2025-07-10 05:01:03 +0900

    원거리공격 및 마우스 포인터로 회전
    원거리 공격 구현 마우스로 회전

--- Quad Action/Assets/Scripts/Player.cs
+++ Quad Action/Assets/Scripts/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     public GameObject[] grenades;
     public int hasGrenade;
 
+
+    public Camera followCamera; //마우스로 카메라 회전
+
     public int ammo;
     public int coin;
     public int health;
@@ -33,14 +36,16 @@ public class Player : MonoBehaviour
     bool jDown;
     bool iDown;
     bool fDown;
+    bool rDown;
     bool sDown1;
     bool sDown2;
     bool sDown3;
-
+    
 
     bool isSwap;
     bool isJump;
     bool isDodge;
+    bool isReload;
     bool isFireReady =true;
 
     Rigidbody rigid;
@@ -74,6 +79,7 @@ public class Player : MonoBehaviour
         Turn();
         Jump();
         Attack();
+        Reload();
         Dodge();
         Interaction();
         Swap();
@@ -86,7 +92,8 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
         jDown = Input.GetButtonDown("Jump");
-        fDown = Input.GetButtonDown("Fire1");
+        fDown = Input.GetButton("Fire1");
+        rDown = Input.GetButtonDown("Reload");
         iDown = Input.GetButtonDown("Interaction");
         sDown1 = Input.GetButtonDown("Swap1");
         sDown2 = Input.GetButtonDown("Swap2");
@@ -104,7 +111,7 @@ public class Player : MonoBehaviour
             moveVec = dodgeVec;
         }
 
-        if (isSwap || !isFireReady)
+        if (isSwap || !isFireReady ||isReload)
         {
             moveVec = Vector3.zero;
         }
@@ -117,7 +124,22 @@ public class Player : MonoBehaviour
 
     void Turn()
     {
+        //키보드에 의한 회전
         transform.LookAt(transform.position + moveVec);
+
+        //마우스에 의한 회전
+        if(fDown)
+        {
+            Ray ray = followCamera.ScreenPointToRay(Input.mousePosition); //마우스가 가리키는 방향으로 ray발사
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit, 100)) //out return처럼 반환값을 주어진 변수에 저장하는 키워드 ray가 읽은거를 rayhit에 저장
+            {
+                Vector3 nextVec = rayHit.point - transform.position; //rayhit포인트는 충돌지점의 월드 좌표
+                nextVec.y = 0; 
+                transform.LookAt(transform.position + nextVec);
+            }
+        }
+       
     }
 
 
@@ -143,9 +165,35 @@ public class Player : MonoBehaviour
         if(fDown && isFireReady &&!isDodge &&!isSwap)
         {
             equipWeapon.Use();
-            anime.SetTrigger("doSwing");
+            anime.SetTrigger(equipWeapon.type == Weapon.Type.Melee ? "doSwing" : "doShot");
             fireDelay = 0;
         }
+    }
+
+    void Reload()
+    {
+        if (equipWeapon == null) return; //손에든게 없으면 리턴
+
+        if (equipWeapon.type == Weapon.Type.Melee) return; //근접무기면 리턴
+
+        if (ammo == 0) return; // 가진 총알이 없으면 리턴
+
+        if(rDown &&!isJump &&!isDodge && !isSwap && isFireReady)
+        {
+            anime.SetTrigger("doReload");
+            isReload = true;
+
+            Invoke("ReloadOut", 2.5f);
+        }
+        
+    }
+
+    void ReloadOut()
+    {
+        int reAmmo = ammo < equipWeapon.maxAmmo ? ammo : equipWeapon.maxAmmo; //가진 총알이 총의 최대 장전 총알보다 작으면 가진 총알을 넣고 많으면 최대치 넣기
+        equipWeapon.curAmmo = reAmmo; //그리고 현재총알을 채워주자
+        ammo -= reAmmo; //그리고 가진총알 빼자
+        isReload = false;
     }
     void Dodge()
     {
--- Quad Action/Assets/Scripts/Weapon.cs
+++ Quad Action/Assets/Scripts/Weapon.cs
@@ -8,8 +8,18 @@ public class Weapon : MonoBehaviour
     public Type type;
     public int damage;
     public float rate;
+
+    public int maxAmmo; //최대 총알 개수
+    public int curAmmo; //지금 총알개수
+
+
     public BoxCollider meleeArea;
     public TrailRenderer trailEffect;
+
+    public Transform bulletPos;
+    public GameObject bullet;
+    public Transform bulletCasePos;
+    public GameObject bulletCase;
 
     public void Use()
     {
@@ -18,6 +28,13 @@ public class Weapon : MonoBehaviour
             StopCoroutine("Swing"); // 코루틴을 작동중에도 멈춘다.
             StartCoroutine("Swing");
         }
+
+        else if (type ==Type.Range && curAmmo >0)
+        {
+            curAmmo--;
+            StartCoroutine("Shot");
+        }
+      
     }
     
     IEnumerator Swing()
@@ -38,4 +55,22 @@ public class Weapon : MonoBehaviour
     }
     //use가 메인루틴 -> swing() 서브 루틴  ->다시 메인루틴으로 *여태까지한것
     //코루틴은 use()에서 swing이 같이 실행된다 -> 코루틴(협동)
+
+    IEnumerator Shot()
+    {
+        //1 총알발사
+        GameObject instantBullet = Instantiate(bullet, bulletPos.position, bulletPos.rotation);
+        Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = bulletPos.forward * 50;
+      
+        yield return null;
+
+        //2 탄피 배출
+        GameObject instantCase = Instantiate(bulletCase, bulletCasePos.position, bulletCasePos.rotation);
+        Rigidbody CaseRigid = instantCase.GetComponent<Rigidbody>();
+        Vector3 caseVec = bulletCasePos.forward * Random.Range(-3, -2)+ Vector3.up*Random.Range(2,3);
+        CaseRigid.AddForce(caseVec, ForceMode.Impulse);
+        CaseRigid.AddTorque(Vector3.up * 10,ForceMode.Impulse);
+
+    }
 }
